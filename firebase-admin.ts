ff2506a0d61facd33e41980fdbb6f5dedf5ac952
Niri <nirl123@gmail.com>
2024-11-27 22:40:59 +0200

    first commit

--- firebase-admin.ts
+++ firebase-admin.ts
@@ -10,8 +10,8 @@ let app: App;
 if (getApps().length === 0) {
   app = initializeApp({
     credential: cert({
-      privateKey:process.env.FIREBASE_PRIVATE_KEY,
-      clientEmail:process.env.FIREBASE_CLIENT_EMAIL,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
       projectId: process.env.FIREBASE_PROJECT_ID,
       
     }),
