80cad1fe520915fcb4faf783fcdee17e72023ec6
Niri <nirl123@gmail.com>
2024-11-27 22:21:27 +0200

    first commit

--- firebase-admin.ts
+++ firebase-admin.ts
@@ -3,13 +3,18 @@ import { initializeApp, getApps, App, getApp, cert } from "firebase-admin/app";
 
 import { getFirestore } from "firebase-admin/firestore";
 
-const serviceKey = require("@/service_key.json");
+// const serviceKey = require("@/service_key.json");
 
 let app: App;
 
 if (getApps().length === 0) {
   app = initializeApp({
-    credential: cert(serviceKey),
+    credential: cert({
+      privateKey:process.env.FIREBASE_PRIVATE_KEY,
+      clientEmail:process.env.FIREBASE_CLIENT_EMAIL,
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      
+    }),
   });
 } else {
   app = getApp();
